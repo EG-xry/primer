dc0e010f82f88379298971720f28ad8390afbc47
Eric GAO <e2gao@ucsd.edu>
2026-05-04 23:17:24 +0800

    Keep only python and docs folders

--- MotorScripts/makefig_302_MotorData_UncoupledGLM.m
+++
@@ -1,9 +0,0 @@
-wd = '../MotorData/';
-load([wd '/all_units.mat']);
-datafile = 'mabel.mat';
-method = 'spg';
-binsize = 0.01;
-dt = .1;
-frames = 80;
-proc = preprocess([wd datafile], binsize, dt, frames);
-plot_filters(ggs, proc, [wd '/uncoupled_filters.eps']);
